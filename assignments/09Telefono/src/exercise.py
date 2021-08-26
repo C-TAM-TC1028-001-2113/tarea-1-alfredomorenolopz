dd5beeeefea694daccd048aa7446299b7f783ae1
Alfredo Moreno <A01411838@itesm.mx>
2021-08-26 10:03:18 -0500

    Actualicé los ejercicios 07, 09 y 10

--- assignments/09Telefono/src/exercise.py
+++ assignments/09Telefono/src/exercise.py
@@ -1,6 +1,13 @@
 def main():
     # escribe tu código abajo de esta línea
-    pass
+    num_msj = int(input("Dame el número de mensajes: "))
+    num_megas = float(input("Dame el número de megas: "))
+    num_min = int(input("Dame el número de minutos: "))
+
+    costo_mensual = (num_msj * 0.80) + (num_megas * 0.80) + (num_min * 0.80)
+
+    print("El costo mensual es:", costo_mensual)
+
 
 
 if __name__ == '__main__':
